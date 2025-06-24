95d98e77d2f83eeac86c8cd84b1923b45267741b
DevMadrine <madrinenamulindwa90@gmail.com>
2025-06-24 09:20:19 +0300

    root for the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
-import App from './App.jsx'
-import Header from "./components/Header.jsx";
-import Main from "./components/Main.jsx";
+import {JournalHeader} from "./components/JournalHeader.jsx";
+import Entry from "./components/Entry.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <Header/>
-      <Main/>
-    <App />
+      <JournalHeader/>
+      <Entry/>
   </StrictMode>,
 )
